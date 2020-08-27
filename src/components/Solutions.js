f805f34bd88b105d9deb23ee28a59a77446c116f
ferschbet <ferschbet@gmail.com>
2020-08-27 14:31:50 -0500

    Más cambios en sección Solutions

--- src/components/Solutions.js
+++ src/components/Solutions.js
@@ -11,47 +11,56 @@ import card3g from "./assets/card3g.png";
 import card3h from "./assets/card3h.png";
 
 const Styles = styled.div`
+  .solutions-container {
+    margin-top: 5rem;
+    max-height: 678px;
+    width: auto;
+  }
   .sol-container1 {
     height: auto;
     width: auto;
-    padding-bottom: 10rem;
+    margin-top: 0;
   }
   .sol-container2 {
+    max-height: 100px;
     height: auto;
     width: auto;
-    padding-bottom: 10rem;
+    /*    padding-bottom: 10rem;*/
   }
   .solutions {
     height: 30px;
-    margin-bottom: 3rem;
-    margin-left: 5rem;
-    margin-top: 5rem;
+    margin-bottom: 5rem;
+    margin-left: auto;
+    margin-right: auto;
   }
   .sol-text {
     text-align: center;
+    color: #282a2b;
+    font-family: Roboto;
+    font-size: 32px;
   }
   .sol-col {
-    max-width: 30rem;
-    margin: auto;
+    width: auto;
+    margin: 0;
   }
   .card3 {
     text-align: center;
-    height: 24.9rem;
+    height: 15rem;
     width: 16.5rem;
+    border: none;
   }
   .cont-img {
-    /*    width: 75%;*/
-    height: auto;
-    /*margin-top: 20px;*/
-    margin-bottom: 80px;
+    width: 75px;
+    height: 75px;
+    margin-bottom: 25px;
   }
   .card3-text {
     color: #282a2b;
     font-family: Roboto;
     font-size: 16px;
     letter-spacing: 0.33px;
-    line-height: 19px;
-    text-align: left;
+    line-height: 24px;
+    text-align: center;
   }
   .card3-button {
     text-align: center;
@@ -63,162 +72,160 @@ const Styles = styled.div`
     }
   }
   @media only screen and (max-width: 800px) {
-    .most-popular {
-      align-items: center;
-      text-align: center;
+    .solutions {
       margin-left: auto;
       margin-right: auto;
     }
     .sol-row {
       align-items: center;
       vertical-align: center;
-      margin-left: auto;
+      margin-left: 3rem;
       margin-right: auto;
     }
     .sol-text {
-      font-size: 36px;
+      font-size: 28px;
+      padding-bottom: 2rem;
     }
   }
 `;
 
-export const MostPopular = () => (
+export const Solutions = () => (
   <Styles>
-    <Container className="sol-container1" fluid>
-      <div className="solutions">
-        <h1 className="sol-text">All of our solutions include </h1>
-      </div>
-      <Row className="sol-row mr-5 mb-5 ml-5">
-        <Col lg={3} md={6} className="sol-col">
-          <Card className="card3 mb-3">
-            <Card.Body>
-              <Card.Img
-                className="cont-img"
-                variant="top"
-                src={card3a}
-                alt="image card3a"
-              />
-              <Card.Text className="card3-text">
-                Forge/OS, enteprise grade operating system for industrial
-                automation
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col lg={3} md={6} className="sol-col">
-          <Card className="card3 mb-3">
-            <Card.Body>
-              <Card.Img
-                className="cont-img"
-                variant="top"
-                src={card3b}
-                alt="image card3b"
-              />
-              <Card.Text className="card3-text">
-                READY Canvas, our award winning, robot agnostic, visual
-                programming interface
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col lg={3} md={6} className="sol-col">
-          <Card className="card3 mb-3">
-            <Card.Body>
-              <Card.Img
-                className="cont-img"
-                variant="top"
-                src={card3c}
-                alt="image card3c"
-              />
-              <Card.Text className="card3-text">
-                Controller, an integration hub running Forge/OS, including 8
-                pneumatic ports, 24V I/O, EtherNet/IP, and Modbus
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col lg={3} md={6} className="sol-col">
-          <Card className="card3 mb-3">
-            <Card.Body>
-              <Card.Img
-                className="cont-img"
-                variant="top"
-                src={card3d}
-                alt="image card3d"
-              />
-              <Card.Text className="card3-text">
-                Tech Support via chat and email
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-    <Container className="sol-container2" fluid>
-      <div className="solutions">
-        <h1 className="sol-text">Most Popular</h1>
-      </div>
-      <Row className="sol-row mr-5 mb-5 ml-5">
-        <Col lg={3} md={6} className="sol-col">
-          <Card className="card3 mb-3">
-            <Card.Body>
-              <Card.Img
-                className="cont-img"
-                variant="top"
-                src={card3e}
-                alt="image card3e"
-              />
-              <Card.Text className="card3-text">
-                Free online, and in our Columbus, OH office, training
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col lg={3} md={6} className="sol-col">
-          <Card className="card3 mb-3">
-            <Card.Body>
-              <Card.Img
-                className="cont-img"
-                variant="top"
-                src={card3f}
-                alt="image card3f"
-              />
-              <Card.Text className="card3-text">
-                Safety Assessment template and support
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col lg={3} md={6} className="sol-col">
-          <Card className="card3 mb-3">
-            <Card.Body>
-              <Card.Img
-                className="cont-img"
-                variant="top"
-                src={card3g}
-                alt="image card3g"
-              />
-              <Card.Text className="card3-text">
-                One year warranty on parts
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col lg={3} md={6} className="sol-col">
-          <Card className="card3 mb-3">
-            <Card.Body>
-              <Card.Img
-                className="cont-img"
-                variant="top"
-                src={card3h}
-                alt="image card3h"
-              />
-              <Card.Text className="card3-text">
-                Solution review prior to order finalization
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+    <Container className="solutions-container">
+      <Container className="sol-container1" fluid>
+        <div className="solutions">
+          <h1 className="sol-text">All of our solutions include </h1>
+        </div>
+        <Row className="sol-row">
+          <Col lg={3} md={6} className="sol-col">
+            <Card className="card3">
+              <Card.Body>
+                <Card.Img
+                  className="cont-img"
+                  variant="top"
+                  src={card3a}
+                  alt="image card3a"
+                />
+                <Card.Text className="card3-text">
+                  Forge/OS, enteprise grade operating system for industrial
+                  automation
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={6} className="sol-col">
+            <Card className="card3">
+              <Card.Body>
+                <Card.Img
+                  className="cont-img"
+                  variant="top"
+                  src={card3b}
+                  alt="image card3b"
+                />
+                <Card.Text className="card3-text">
+                  READY Canvas, our award winning, robot agnostic, visual
+                  programming interface
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={6} className="sol-col">
+            <Card className="card3">
+              <Card.Body>
+                <Card.Img
+                  className="cont-img"
+                  variant="top"
+                  src={card3c}
+                  alt="image card3c"
+                />
+                <Card.Text className="card3-text">
+                  Controller, an integration hub running Forge/OS, including 8
+                  pneumatic ports, 24V I/O, EtherNet/IP, and Modbus
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={6} className="sol-col">
+            <Card className="card3">
+              <Card.Body>
+                <Card.Img
+                  className="cont-img"
+                  variant="top"
+                  src={card3d}
+                  alt="image card3d"
+                />
+                <Card.Text className="card3-text">
+                  Tech Support via chat and email
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      <Container className="sol-container2" fluid>
+        <Row className="sol-row">
+          <Col lg={3} md={6} className="sol-col">
+            <Card className="card3">
+              <Card.Body>
+                <Card.Img
+                  className="cont-img"
+                  variant="top"
+                  src={card3e}
+                  alt="image card3e"
+                />
+                <Card.Text className="card3-text">
+                  Free online, and in our Columbus, OH office, training
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={6} className="sol-col">
+            <Card className="card3">
+              <Card.Body>
+                <Card.Img
+                  className="cont-img"
+                  variant="top"
+                  src={card3f}
+                  alt="image card3f"
+                />
+                <Card.Text className="card3-text">
+                  Safety Assessment template and support
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={6} className="sol-col">
+            <Card className="card3">
+              <Card.Body>
+                <Card.Img
+                  className="cont-img"
+                  variant="top"
+                  src={card3g}
+                  alt="image card3g"
+                />
+                <Card.Text className="card3-text">
+                  One year warranty on parts
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={6} className="sol-col">
+            <Card className="card3">
+              <Card.Body>
+                <Card.Img
+                  className="cont-img"
+                  variant="top"
+                  src={card3h}
+                  alt="image card3h"
+                />
+                <Card.Text className="card3-text">
+                  Solution review prior to order finalization
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   </Styles>
 );
